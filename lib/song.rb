ba018c0a8a2dd2678d797015df126d59ef09c78f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-08 02:45:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,8 @@ class Song
 
   def new_by_filename(filename)
     filename.each do |file|
-      split_file = file.
+      split_file = file.split(" - ")
+      artist = split_file[0]
+      title = split_file[1]
   end
 end
